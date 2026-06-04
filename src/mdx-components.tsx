69f3c48d41a8952d42e3adb8c345c62395497265
Rodney Osodo <socials@rodneyosodo.com>
2026-06-04 19:33:17 +0300

    refactor(mdx-components): simplify img component and remove assetPath
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,10 +2,8 @@ import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-import type { ComponentPropsWithoutRef } from "react";
 import { APIPage } from "@/components/api-page";
 import { CodeFromSource } from "@/components/code-from-source";
-import { assetPath } from "@/lib/base-path";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
@@ -17,14 +15,7 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
         <Pre>{props.children}</Pre>
       </CodeBlock>
     ),
-    img: ({ src, ...props }: ComponentPropsWithoutRef<"img">) => {
-      return (
-        <ImageZoom
-          {...props}
-          src={typeof src === "string" ? assetPath(src) : undefined}
-        />
-      );
-    },
+    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
   };
 }
